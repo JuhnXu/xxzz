3c6872060a246d4a64b1834c542649529f6a1af9
JuhnXu <JuhnXu@qq.com>
2013-05-15 01:02:24 +0800

    Initial Commit

---
+++ xxz/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  xxzAppController.h
+//  xxz
+//
+//  Created by JuhnXu on 13-5-15.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
